a8329545760bd3369cb5f514e82ce70e1075a7bd
Toufiquer <toufiquerabc@gmail.com>
2023-11-04 03:05:06 +0600

    done Init project

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+/*
+|-----------------------------------------
+| setting up Page for the App
+| @author: Toufiquer Rahman<toufiquer.0@gmail.com>
+| @copyright: Toufiquer, November, 2023
+|-----------------------------------------
+*/
+const Page = () => {
+  return (
+    <main className="flex w-full h-screen items-center justify-center text-6xl">
+      Blog Page
+    </main>
+  );
+};
+export default Page;
